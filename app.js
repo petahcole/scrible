bf92bf3fadc564785a1e9e360bc2b09d23409543
Cole Batson <cbatson89@gmail.com>
2016-11-14 16:51:44 -0700

    added reddit call to prose button

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-    $("#poetry").click(function() {
-      $(".prompt").html("")
-        // event.preventDefault();
-        $.get("https://www.reddit.com/r/POETRYPrompts/new.json", getPoetryPrompt)
+
+    $("#poetry").click(function(event) {
+        event.preventDefault();
+        $(".prompt").html("");
+        $.get("https://www.reddit.com/r/POETRYPrompts/new.json", getPoetryPrompt);
     });
 
+    $("#prose").click(function(event) {
+        event.preventDefault();
+        $(".prompt").html("");
+        $.get("https://www.reddit.com/r/WritingPrompts/top.json", getProsePrompt);
+
+    })
 })
 
 function getPoetryPrompt(poetryData) {
@@ -13,6 +20,18 @@ function getPoetryPrompt(poetryData) {
         poetryPrompts.push(poetryData.data.children[i].data.title);
     }
     var randomPoetryPrompt = poetryPrompts[Math.floor(Math.random() * poetryPrompts.length)];
-    var randomPoetryPrompt = randomPoetryPrompt.replace("[PP]","")
+    randomPoetryPrompt = randomPoetryPrompt.replace("[PP]", "");
     $(".prompt").append("<h5>'" + randomPoetryPrompt + " '</h5>");
+};
+
+function getProsePrompt(proseData)  {
+    var prosePrompts = [];
+
+    for (var i = 0; i < proseData.data.children.length; i++) {
+        prosePrompts.push(proseData.data.children[i].data.title);
+    }
+    var randomProsePrompt = prosePrompts[Math.floor(Math.random() * prosePrompts.length)];
+    randomProsePrompt = randomProsePrompt.replace("[WP]", "");
+    $(".prompt").append("<h5>'" + randomProsePrompt + "'</h5>")
+
 }
