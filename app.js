fc6b44205f3f67a2454a3c096b2452832a220c9c
Cole Batson <cbatson89@gmail.com>
2016-11-14 14:20:33 -0700

    poetry button returns random prompt

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
-  $("#poetry").click(function() {
-    // event.preventDefault();
-    $.get("https://www.reddit.com/r/POETRYPrompts/new.json", getPoetryPrompt)
-  });
+    $("#poetry").click(function() {
+        // event.preventDefault();
+        $.get("https://www.reddit.com/r/POETRYPrompts/new.json", getPoetryPrompt)
+    });
 
 })
 
-function getPoetryPrompt(poetryData)  {
-  console.log(poetryData.data.children[0].data.title);
-  //loop through .children[i]
-  //push data.title into an array
-  //randomize the array
-  //print random prompt
-  
-
+function getPoetryPrompt(poetryData) {
+    var poetryPrompts = []
+    for (var i = 0; i < poetryData.data.children.length; i++) {
+        poetryPrompts.push(poetryData.data.children[i].data.title);
+    }
+    var randomPoetryPrompt = poetryPrompts[Math.floor(Math.random() * poetryPrompts.length)];
+    $("main").append("<h5>'" + randomPoetryPrompt + "'</h5>");
 }
